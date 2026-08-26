2490abc0a74f8008f635a8cde832aa3d3de588e6
olwethu28 <137727115+Olwethu28@users.noreply.github.com>
2026-08-26 10:37:27 +0200

    fix: restructuring my project folder structure

--- fitness-tracker/vite.config.js
+++ fitness-tracker/vite.config.js
@@ -1,7 +1,21 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      thresholds: {
+        lines: 70,
+        branches: 70,
+        functions: 70,
+        statements: 70
+      }
+    }
+  }
+});
